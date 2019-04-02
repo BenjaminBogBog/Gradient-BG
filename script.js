d7a478303912e3d59b176551814fbe28c569c8e9
BenjaminBogBog <44836216+BenjaminBogBog@users.noreply.github.com>
2019-04-02 17:16:36 +0700

    revert

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 const body = document.querySelector("#gradient");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
-const hex = document.querySelector('#Hex');
-const rgb = document.querySelector('#rgb');
 
 var SetGradient = () => {
   body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-
-  hex.textContent = "Hex Color Value: " + color1.value + ", " + color2.value;
-  rgb.textContent = "CSS Property: " + body.style.background + ";";
-
 }
 
 color1.addEventListener("input", SetGradient);
