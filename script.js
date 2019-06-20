3b3a36270654196cb10d3286ba7f7d31648cc8a0
BenjaminBogBog <44836216+BenjaminBogBog@users.noreply.github.com>
2019-06-20 21:52:00 +0700

    Added Credits & Animation

--- script.js
+++ script.js
@@ -3,11 +3,23 @@ const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const hex = document.querySelector('#Hex');
 const css = document.querySelector('#CSS');
+const boganim = document.querySelector('#boganim');
+const button = document.querySelector('.play')
 
 var SetGradient = () => {
   body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
   hex.textContent = "Hex Color: " + color1.value + ", " + color2.value;
   css.textContent = "CSS Property: " + body.style.background + ";";
+  button.style.background = "linear-gradient(to right, " + color2.value + ", " + color1.value + ")";
+}
+
+var startAnim = () => {
+  boganim.className = "bogbog";
+    button.disabled = true;
+  setTimeout(function () {
+    button.disabled = false;
+    boganim.className = "";
+  }, 4000);
 }
 
 color1.addEventListener("input", SetGradient);
